18c806ec392e855f3eaca6a1b0837e64674c4f00
MohannadAljundi <61085040+MohannadAljundi@users.noreply.github.com>
2020-02-15 16:06:45 +0200

    update

--- FirstApp/app/src/main/java/com/yasmen/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/yasmen/firstapp/MainActivity.java
@@ -1,14 +1,14 @@
 package com.yasmen.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+         import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+         import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_layout);
     }
 }

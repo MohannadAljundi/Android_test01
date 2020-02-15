4dfc241362b3d78189f9753bd9a0eeab38fac708
MohannadAljundi <61085040+MohannadAljundi@users.noreply.github.com>
2020-02-15 17:05:29 +0200

    Update MainActivity.java

--- FirstApp/app/src/main/java/com/yasmen/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/yasmen/firstapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.yasmen.firstapp;
          import androidx.appcompat.app.AppCompatActivity;
 
          import android.os.Bundle;
+         import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_layout);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setText("Hellow");
     }
 }
